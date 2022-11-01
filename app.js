2cbc215437727ad34548e4adea92346e86c6eff2
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-11-01 18:29:15 +0530

    Remove unnecessory parameters

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+let port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Success");
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
   console.log(`Server listning on at http://localhost:${port}`);
 });
