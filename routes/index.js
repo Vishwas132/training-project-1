75acc7a7b63eb343462e1c4c97b808e6ff5ec198
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2022-11-06 19:43:01 +0530

    Create user route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import userRoute from "./userRoute.js";
 
 const route = Router();
 
 route.get("/", (req, res) => {
   res.send("Success");
 });
+
+route.use("/user", userRoute);
 
 export default route;
